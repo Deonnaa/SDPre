f254fc685a44ac88274a325b863efd89b46f8921
Deonnaa <aponte.deonna@gmail.com>
2023-09-12 20:41:08 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    /*
+      String name = "Deonna";
+      int age = 26;
+      String state = "North Carolina";
+    */
+    
+  }
+}
