7a580d567297c4046068a10d47e6a3cc2c8a96a2
Deonnaa <aponte.deonna@gmail.com>
2023-09-12 20:41:08 -0400

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    /*
+      int yearBorn = 1997;
+      String monthBorn = "May";
+      boolean isWeekend = false;
+    */
+
+  }
+}
