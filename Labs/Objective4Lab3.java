d37fbe76321c0d751fa0765b3df861abecbdc55f
Deonnaa <aponte.deonna@gmail.com>
2023-09-18 12:53:00 -0400

    complete objective4lab3

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -5,8 +5,7 @@ public class Objective4Lab3 {
   public static void main(String[] args) {
     int birthYear, age, currentYear;
 
-    birthYear = (currentYear - age);
-    currentYear = 2023;
+    birthYear = (2023 - age);
 
     Scanner input = new Scanner(System.in);
     System.out.println("How old are you?");
