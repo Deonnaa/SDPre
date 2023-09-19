a2db80d6068d7bbabf3452426b71c21e09be1f93
Deonnaa <aponte.deonna@gmail.com>
2023-09-19 18:40:25 -0400

    complete objective6lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,21 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int month = 0;
+
+    switch (month) {
+      case 1:
+        System.out.println("January");
+        break;
+
+      /*
+        case 6:
+        System.out.println("June");
+        break;
+      */
+
+      default:
+        System.out.println("That is not a valid month");
+        break;
+    }
+  }
+}
