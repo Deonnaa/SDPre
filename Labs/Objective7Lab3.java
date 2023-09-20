e80232003161d0492370d2061207512e9f4e31c4
Deonnaa <aponte.deonna@gmail.com>
2023-09-20 19:23:15 -0400

    complete objective7lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,21 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter <= 20) {
+      
+    counter = counter + 1;
+
+    if(num % 2 == 0) {
+        System.out.println(counter + " is even");
+        } 
+      else {
+        System.out.print(counter + " is odd");
+        }
+
+    /*
+      Your solution goes here
+    */
+    
+  }
+}
