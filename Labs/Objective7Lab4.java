8e5c472b52e2febc61ee3f4453620c924b54df03
Deonnaa <aponte.deonna@gmail.com>
2023-09-20 19:55:31 -0400

    complete objective7lab4

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -1,16 +1,15 @@
 public class Objective7Lab4 {
 	public static void main(String[] args) {
 		
-		int counter = 1;
+		int counter = 0;
 		int sum = 0;
 
 		while (counter <= 20) {
       	counter = counter + 1;
       	sum = counter + sum;
-		}
-      	
+		
       	System.out.println(sum);
-
+		}
 
 
 
