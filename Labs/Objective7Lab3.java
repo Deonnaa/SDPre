98e6e7bb8915baf86d33a0c91fb90f776129eb34
Deonnaa <aponte.deonna@gmail.com>
2023-09-20 19:37:51 -0400

    complete objective7lab3

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -3,15 +3,15 @@ public class Objective7Lab3 {
     int counter = 1;
 
     while(counter <= 20) {
-      //System.out.println(counter);
+      System.out.println(counter);
       counter = counter + 1;
       }
 
       if (counter % 2 == 0){
-        System.out.println(counter + "is even");
+        System.out.println(counter + " is even");
       }
       else {
-        System.out.println(counter + "is odd");
+        System.out.println(counter + " is odd");
       }
 
       /*
