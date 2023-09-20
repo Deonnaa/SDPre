0ec6ffc75877b3ef32355bd515be43f3c9dbbd5f
Deonnaa <aponte.deonna@gmail.com>
2023-09-20 19:39:35 -0400

    complete objective7lab3

--- Labs/Objective7Lab3.java
+++ Labs/Objective7Lab3.java
@@ -1,6 +1,6 @@
 public class Objective7Lab3 {
   public static void main(String[] args) {
-    int counter = 1;
+    int counter = 0;
 
     while(counter <= 20) {
       //System.out.println(counter);
