39e2fc1810f0f511201d629f564797dabe2ec3b7
Deonnaa <aponte.deonna@gmail.com>
2023-09-22 13:18:08 -0400

    complete objective8lab3

--- Labs/Objective8Lab3.java
+++ Labs/Objective8Lab3.java
@@ -2,7 +2,7 @@ public class Objective8Lab3 {
   public static void main(String[] args) {
     int counter = 1;
 
-    for (int i=0; i<20; i--) {
+    for (int i=1; i<=20; i--) {
 
       if (counter % 2 == 0) {
         System.out.println(counter + " is even");
