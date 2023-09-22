b5b112019665b6ad34a77bf393bcfa7fb084ccd1
Deonnaa <aponte.deonna@gmail.com>
2023-09-22 12:37:43 -0400

    complete objective8lab1

--- Labs/Objective8Lab1.java
+++ Labs/Objective8Lab1.java
@@ -2,7 +2,7 @@ public class Objective8Lab1 {
   public static void main(String[] args) {
     
     // TODO Write a for loop that will print the numbers 1 to 10
-    for (int i=0; i<10; i+=1){
+    for (int i=1; i<11; i+=1) {
       System.out.println(i);  
       }
 
